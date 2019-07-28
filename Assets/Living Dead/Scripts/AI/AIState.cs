6d2001343554c90f68ebbfb05099966144c8d475
Lloyd Risper <lloydrisper@me.com>
2019-07-27 20:45:01 -0400

    Patrol State

--- Assets/Living Dead/Scripts/AI/AIState.cs
+++ Assets/Living Dead/Scripts/AI/AIState.cs
@@ -72,4 +72,19 @@ public abstract class AIState : MonoBehaviour
 
         radius = Mathf.Max(radius, col.radius * col.transform.lossyScale.z);
     }
+
+    // -----------------------------------------------------------------------
+    // Name :   FindSignedAngle
+    // Desc :   Returns the signed angle between to vectors (in degrees)
+    // -----------------------------------------------------------------------
+    public static float FindSignedAngle(Vector3 fromVector, Vector3 toVector)
+    {
+        if (fromVector == toVector)
+            return 0.0f;
+
+        float angle = Vector3.Angle(fromVector, toVector);
+        Vector3 cross = Vector3.Cross(fromVector, toVector);
+        angle *= Mathf.Sign(cross.y);
+        return angle;
+    }
 }
